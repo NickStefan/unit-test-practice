f16698b0c34e044a637bebe8c66d346e7b14104d
Nick Stefan <NickStefan12@gmail.com>
2014-08-28 19:19:57 -0700

    wrote test for a silly function. not implimented yet.

--- spec/nsSpec.js
+++ spec/nsSpec.js
@@ -6,7 +6,19 @@ describe('arrayCount', function(){
   it('should return a count of the number of array elements', function() {
     expect(ns.arrayCount([1,2,6])).to.equal(3);
     expect(ns.arrayCount([])).to.equal(0);
-    expect(ns.arrayCount()).to.equal(undefined);
     expect(ns.arrayCount(arr)).to.equal(arr.length);
+  });
+  
+  it('should return a number', function(){
+    expect(ns.arrayCount([])).to.be.a('number');
+    expect(ns.arrayCount(arr)).to.be.a('number');
+    
+  });
+  
+});
+
+describe('fancyName', function(){
+  it('should return a name of at least 4 parts', function(){
+    expect(ns.fancyName("Nick Stefan").split(" ").length).to.be.at.least(4);
   });
 });
