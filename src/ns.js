794301c09bbe6d20d801693c695a439605b69562
Nick Stefan <NickStefan12@gmail.com>
2014-08-28 19:32:36 -0700

    passed test on silly function

--- src/ns.js
+++ src/ns.js
@@ -1,7 +1,22 @@
 ns = {};
 
 (function(){
+  
   ns.arrayCount = function(arr){
     return (arr ? arr.length : undefined);
   };
+  
+  ns.fancyName = function(str){
+    titles = ["Lord","King",""];
+    middles = ["Barthollemew","Charles"];
+    numerals = ["I","II","III","IV"];
+    str = str.split(' ');
+
+    str.unshift(titles[Math.floor(Math.random() * titles.length)]);
+    str.splice(2,0,middles[Math.floor(Math.random() * middles.length)]);
+    str.push(numerals[Math.floor(Math.random() * numerals.length)]);
+    console.log(str);
+    return str.join(" ");
+  };
+  
 }).call(this);
